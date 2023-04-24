64014fbe8fd1e4995bcb00b49bf554d131bec451
Yu357 <forestgreen14689@gmail.com>
2023-04-24 14:55:45 +0900

    feat: Aboutページに文章を配置

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,6 +9,17 @@ function about() {
 			</Head>
 		
 			<h1>About</h1>
+
+			<div className="mt-4">
+
+				<p>Lorem ipsum dolor sit amet consectetur.</p>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit fuga saepe aliquid earum vel aut! Vitae quidem obcaecati numquam, in praesentium magnam! Autem explicabo quo optio quam nulla a quis.</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa aspernatur suscipit autem, similique, quibusdam nemo voluptatum ea, consequatur facilis tempore dignissimos provident nesciunt quas laborum quidem. Molestias aliquid atque iste repudiandae magni doloribus minima suscipit eveniet odio quasi, eum in, cupiditate earum ab provident voluptatum totam adipisci aperiam incidunt accusamus? Molestias, iusto. Officiis illum alias aliquam, fuga voluptate iusto molestias tempore mollitia velit quas atque cumque fugit neque, ratione labore eveniet aperiam voluptas beatae animi dolores ipsum consequatur repudiandae doloremque reprehenderit? Dolorum minima sapiente officiis cupiditate quam modi nobis minus excepturi quisquam ipsam, ullam aut labore possimus non, iure porro molestiae pariatur? Est dolores voluptas ipsum accusantium praesentium voluptatem eos quaerat, in earum fuga enim nesciunt placeat quibusdam asperiores nihil quam neque mollitia iste, sunt aut culpa. Id animi sequi quia reprehenderit molestias nemo impedit asperiores distinctio, officia voluptate, ducimus necessitatibus, eligendi accusamus? Quisquam eligendi ut nam, dolor animi molestias? Eveniet perferendis mollitia tempora ipsa ratione nobis perspiciatis unde, dolorem commodi sequi odit sunt maxime doloremque voluptate obcaecati ullam impedit enim quod iure in. Facilis molestiae rem tempora eos dignissimos eveniet explicabo hic, vel odit ratione suscipit temporibus, molestias vero sequi doloremque, illo ipsa! Voluptatum quia nam nulla in aliquid?</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque cumque sapiente dolores deleniti repellendus earum quibusdam unde ratione, et reiciendis velit nisi numquam esse quos delectus dolor neque cupiditate. Enim a dolorem consequuntur eaque! Vel velit cum dolore necessitatibus quo.</p>
+				<p>Lorem ipsum dolor sit.</p>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident fugit ullam adipisci. Laborum ut quia veniam velit cupiditate odio eos!</p>
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ipsum dolorum amet non, reiciendis explicabo cum ab aut sint nam voluptatem hic. Molestiae, quidem.</p>
+			</div>
 		</main>
 	);
 }
