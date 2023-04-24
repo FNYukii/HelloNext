1fdb87677c2693f86384f39a7c586d94c0f1aeea
Yu357 <forestgreen14689@gmail.com>
2023-04-24 14:17:15 +0900

    feat: Headerをレスポンシブに

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,15 @@ function Header() {
 
 		<header>
 
-			<div>
+			<div className="mx-auto w-full lg:width-lg px-4 lg:px-0 py-4 flex justify-between">
 
-				<Link href="/" className="font-bold text-2xl">Hello Next</Link>
+				<Link href="/" className="text-4xl">Hello Next</Link>
 
-				<div>
+				<div className="flex items-center gap-4">
 
-					<Link href="/about">About</Link>
-					<Link href="/contact">Contact</Link>
+					<Link href="/" className="hover:underline">Top</Link>
+					<Link href="/about" className="hover:underline">About</Link>
+					<Link href="/contact" className="hover:underline">Contact</Link>
 				</div>
 			</div>
 		</header>
