12580df9ae627bc6f7982eb71ba92527de8e5867
Yu357 <forestgreen14689@gmail.com>
2023-04-24 14:52:31 +0900

    feat: 404ページができた

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+function NotFound() {
+	
+	return (
+		<main className="mx-auto w-full lg:width-lg px-4 lg:px-0">
+			
+			<Head>
+				<title>Page Not Found - Hello Next</title>
+			</Head>
+		
+			<h1>Page Not Found</h1>
+		</main>
+	);
+}
+
+export default NotFound;
