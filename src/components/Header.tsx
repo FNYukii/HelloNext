a39a61d2f6f7c8238ec9d5ea9ffb29e3adc9c18d
Yu357 <forestgreen14689@gmail.com>
2023-05-18 11:45:36 +0900

    feat: ページの構成を刷新

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,8 +16,8 @@ function Header() {
 				<div className="flex items-center gap-4">
 
 					<Link href="/" className={router.pathname == "/" ? "" : "text-gray-500 hover:text-black"}>Top</Link>
-					<Link href="/about" className={router.pathname == "/about" ? "" : "text-gray-500 hover:text-black"}>About</Link>
-					<Link href="/contact" className={router.pathname == "/contact" ? "" : "text-gray-500 hover:text-black"}>Contact</Link>
+					<Link href="/posts" className={router.pathname == "/posts" ? "" : "text-gray-500 hover:text-black"}>Posts</Link>
+					<Link href="/todos" className={router.pathname == "/todos" ? "" : "text-gray-500 hover:text-black"}>Todos</Link>
 				</div>
 			</div>
 		</header>
---
+++ src/pages/todos.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head"
+
+function todos() {
+	
+	return (
+		<>
+
+			<Head>
+				<title>Todos - Hello Next</title>
+			</Head>
+
+			<main className="mx-auto w-full lg:width-lg px-4 lg:px-0">
+
+				<h1 className="text-2xl font-bold">Todos</h1>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+			</main>
+		</>
+	)
+}
+
+export default todos
