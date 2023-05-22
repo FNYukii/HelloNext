82efd1a014bd8f0233ae3a2149e19def1852e617
Yu357 <forestgreen14689@gmail.com>
2023-05-22 15:33:04 +0900

    feat: firebaseConfig.tsを作成

---
+++ src/utilities/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAEA6h3Sd0VljN9rmzjUoZuXtplV1G8aTk",
+  authDomain: "hellonext-864c0.firebaseapp.com",
+  projectId: "hellonext-864c0",
+  storageBucket: "hellonext-864c0.appspot.com",
+  messagingSenderId: "814298879697",
+  appId: "1:814298879697:web:d2683b04fd98b5554257f0",
+  measurementId: "G-SFKZWEC72L"
+}
