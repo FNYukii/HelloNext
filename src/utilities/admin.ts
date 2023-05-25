ab89f8dd8d1d0a86c3c9ab57d92673efa6b161dc
Yu357 <forestgreen14689@gmail.com>
2023-05-25 11:06:41 +0900

    feat: update admin.ts

--- src/utilities/admin.ts
+++ src/utilities/admin.ts
@@ -7,8 +7,8 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
 
       projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   })
 }
