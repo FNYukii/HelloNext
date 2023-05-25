aeae30a8f2695667ed46c40ebf869774d23fb38b
Yu357 <forestgreen14689@gmail.com>
2023-05-25 11:11:50 +0900

    feat

--- src/utilities/admin.ts
+++ src/utilities/admin.ts
@@ -2,12 +2,15 @@ import admin from 'firebase-admin'
 
 if (!admin.apps.length) {
 
+	let privateKey = process.env.FIREBASE_PRIVATE_KEY
+	privateKey = privateKey.replace(/\\n/g, '\n')
+
   admin.initializeApp({
 
     credential: admin.credential.cert({
 
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: privateKey,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   })
