2988b2394cb760f3898fe30b897fd31aab3a05c5
Yu357 <forestgreen14689@gmail.com>
2023-05-25 10:42:30 +0900

    feat: ツイート一覧をSSGで表示できた

--- src/utilities/TweetService.ts
+++ src/utilities/TweetService.ts
@@ -1,27 +1,27 @@
 import Tweet from '../entities/Tweet'
 import { collection, query, getDocs } from "firebase/firestore"
-import { db } from "../utilities/firebase"
+import { db } from "../utilities/admin"
 
 export class TweetService {
 
 	static async readTweets(): Promise<Tweet[]> {
 
-		// usersコレクション内のドキュメントを読み取り
-		const q = query(collection(db, "tweets"))
-		const querySnapshot = await getDocs(q)
+		const tweets: Tweet[] = [];
 
-		// 配列usersを作成
-		let tweets: Tweet[] = []
-		querySnapshot.forEach((doc) => {
+		const ref = await db.collection('tweets').get();
+
+		ref.docs.map((doc) => {
 
 			const id = doc.id
 			const displayName = doc.data().displayName
 			const text = doc.data().text
-			const createdAt: Date = doc.data({ serverTimestamps: "estimate" }).createdAt.toDate() ?? new Date()
 
-			const tweet: Tweet = { id: id, displayName: displayName, text: text, createdAt: createdAt }
-			tweets.push(tweet)
-		})
+			const createdAt: Date = doc.data().createdAt.toDate() ?? new Date()
+
+			const tweet: Tweet = { id: id, displayName: displayName, text: text, createdAt: createdAt.toString() }
+			tweets.push(tweet);
+		});
+
 
 		return tweets
 	}
