f7671e49b4d04cfbdf9e20f884199d37aaf0c37f
Yu357 <forestgreen14689@gmail.com>
2023-05-25 10:13:16 +0900

    chore: firebaseConfigをGit管理対象から削除

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from "firebase/firestore"
 
-import { firebaseConfig } from "./firebaseConfig"
+import firebaseConfig from "./firebaseConfig"
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
