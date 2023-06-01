92ffa6d23a3b94fa16fcfafedc90be00b956e434
Yu357 <forestgreen14689@gmail.com>
2023-06-01 11:50:45 +0900

    feat: フロントエンドからFirestoreにデータを追加する関数を作成

--- src/utilities/TweetService.ts
+++ src/utilities/TweetService.ts
@@ -1,5 +1,8 @@
 import Tweet from '../entities/Tweet'
 import { adminDb } from "./firebaseAdmin"
+import { db } from "./firebase"
+import { addDoc, collection, deleteDoc, doc, endAt, getDocFromCache, getDocFromServer, getDocs, getDocsFromCache, getDocsFromServer, limit, orderBy, query, QueryDocumentSnapshot, serverTimestamp, startAt, where } from "firebase/firestore"
+import { strict } from 'assert';
 
 export class TweetService {
 
@@ -25,8 +28,33 @@ export class TweetService {
 		return tweets
 	}
 
-	static async createTweet(displayName: string, text: string): Promise<string> {
+	static async createTweet(displayName: string, text: string): Promise<string | null> {
 
-		return ""
+		// displayNameのチェック
+		if (displayName.length === 0 || displayName.length > 30) {
+			return null
+		}
+
+		// textのチェック
+		if (text.length === 0 || text.length > 100) {
+			return null
+		}
+
+		try {
+
+			const ref = await addDoc(collection(db, "tweets"), {
+
+				createdAt: serverTimestamp(),
+				displayName: displayName,
+				text: text
+			})
+
+			return ref.id
+
+		} catch (error) {
+
+			console.log(`Failed to tweet creation. ${error}`)
+			return null
+		}
 	}
 }
