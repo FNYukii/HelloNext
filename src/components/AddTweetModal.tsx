68e1d588e3a81f2a56b57ff19f6c7e7c3c17a902
Yu357 <forestgreen14689@gmail.com>
2023-06-01 11:11:35 +0900

    feat: AddTweetModalのボタンにtransitionを設定

--- src/components/AddTweetModal.tsx
+++ src/components/AddTweetModal.tsx
@@ -29,7 +29,7 @@ function AddTweetModal(props: Props) {
 			<div className="absolute bg-white p-8 md:width-600 w-11/12 max-height-screen-90 overflow-y-auto space-y-4">
 
 				<button onClick={() => props.setIsOpenModal(false)}>
-					<MdClose className="text-gray-500 text-3xl hover:opacity-80" />
+					<MdClose className="text-gray-500 text-3xl hover:opacity-60 transition" />
 				</button>
 
 				<input type="text" placeholder="名前" className="w-full py-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500 placeholder:text-gray-400" />
@@ -38,7 +38,7 @@ function AddTweetModal(props: Props) {
 
 				<div className="flex justify-end">
 
-					<button className="py-2 px-4 bg-black text-white hover:opacity-80">
+					<button className="py-2 px-4 bg-black text-white hover:opacity-60 transition">
 						<span>投稿</span>
 					</button>
 				</div>
