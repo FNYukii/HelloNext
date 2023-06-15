897ecd410bfa01d92b88155cb85245e0a4bfbd59
Yu357 <forestgreen14689@gmail.com>
2023-06-15 12:05:34 +0900

    feat: 環境変数名を調整

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore"
 
 // .envファイルに書いてある環境変数を元に、Firebaseプロジェクトの構成情報をまとめる
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_APP_ID,
-	measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // 構成情報を元に、Firebaseを初期化
