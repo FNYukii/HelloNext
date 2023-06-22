e49260d1056b760d948020141a7408e55d86313d
Yu357 <forestgreen14689@gmail.com>
2023-06-22 12:26:32 +0900

    feat: Todosリアルタイムアップデートを実装

--- src/components/TodosSection.tsx
+++ src/components/TodosSection.tsx
@@ -4,26 +4,79 @@ import TodoService from "@/utilities/TodoService";
 import { useState, useEffect } from "react";
 import { MdOutlineClose } from "react-icons/md";
 import AddTodoModal from "./AddTodoModal";
+import { db } from "@/utilities/firebase";
+import { query, collection, where, orderBy, limit, onSnapshot } from "firebase/firestore";
 
 function TodosSection() {
 
 	const [todos, setTodos] = useState<Todo[] | null>(null)
-	const [isLoaded, setIsLoades] = useState(false)
+	const [isLoaded, setIsLoaded] = useState(false)
 
 	const [isShowModal, setIsShowModal] = useState(false)
 
-	async function read() {
+	async function listen() {
 
-		// record一覧を読み取り
-		const todos: Todo[] | null = await TodoService.readTodos()
-		setTodos(todos)
-		setIsLoades(true)
+		// UserIDを取得
+		const userId = await AuthService.uidFromLocalStorage()
+
+		// 未ログインなら、エラーとする
+		if (userId === null) {
+
+			console.log("Fail! Error to listen todos. 未ログイン状態です。")
+			setIsLoaded(true)
+			return
+		}
+
+		// 読み取りクエリを作成
+		const q = query(collection(db, "todos"), where("userId", "==", userId), orderBy("createdAt", "desc"), limit(100))
+
+		// リアルタイムリスナーを設定
+		onSnapshot(q, async (querySnapshot) => {
+
+			// TODO: このコードの意味を調べる
+			if (querySnapshot.metadata.hasPendingWrites) return
+
+			// Todoの配列を作成
+			let todos: Todo[] = []
+			querySnapshot.forEach((doc) => {
+
+				// ドキュメントの各フィールドの値を取り出す
+				const id: string = doc.id ?? ""
+				const userId: string = doc.data().userId ?? ""
+
+				const text: string = doc.data().text ?? ""
+
+				const createdAt: Date = doc.data({ serverTimestamps: "estimate" }).createdAt.toDate() ?? new Date()
+
+				// 値を使ってTodoオブジェクトを作成
+				const todo: Todo = {
+					id: id,
+					userId: userId,
+					text: text,
+					createdAt: createdAt
+				}
+
+				// 配列に追加
+				todos.push(todo)
+			})
+
+			// Stateを更新
+			setTodos(todos)
+			setIsLoaded(true)
+
+		}, (error) => {
+
+			// エラーならログ出力 & State更新
+			console.log(`Comments reading failed. ${error}`)
+			setIsLoaded(true)
+		})
 	}
 
 	useEffect(() => {
 
-		read()
-	}, []);
+		listen()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
 	return (
 
