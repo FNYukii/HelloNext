8f60580ab29fefd7cab4290448f83e5a177e76bc
Yu357 <forestgreen14689@gmail.com>
2023-06-22 11:55:03 +0900

    feat: Todo作成モーダルを表示した

--- src/components/TodosSection.tsx
+++ src/components/TodosSection.tsx
@@ -2,11 +2,14 @@ import Todo from "@/entities/Todo";
 import AuthService from "@/utilities/AuthService";
 import TodoService from "@/utilities/TodoService";
 import { useState, useEffect } from "react";
+import { MdOutlineClose } from "react-icons/md";
 
 function TodosSection() {
 
 	const [todos, setTodos] = useState<Todo[] | null>(null)
 	const [isLoaded, setIsLoades] = useState(false)
+
+	const [isShowModal, setIsShowModal] = useState(false)
 
 	async function read() {
 
@@ -25,18 +28,44 @@ function TodosSection() {
 
 		<div>
 
-			<div className="flex justify-between items-center">
+			<section className="flex justify-between items-center">
 
 				<h1 className="text-2xl font-bold">Todos</h1>
 
 				<div className="flex gap-4">
 
-					<button className="font-bold hover:opacity-60 transition">New Todo</button>
+					<button onClick={() => setIsShowModal(true)} className="font-bold hover:opacity-60 transition">New Todo</button>
 					<button onClick={() => AuthService.signOut()} className="font-bold text-red-500 hover:opacity-60 transition">Sign out</button>
 				</div>
-			</div>
+			</section>
 
-			<div>
+			<section>
+
+				{isShowModal &&
+
+					<div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center dark:text-white">
+
+						<div onClick={() => setIsShowModal(false)} className="w-full h-full bg-black/30 dark:bg-white/20"></div>
+
+						<div className="absolute bg-white rounded-xl md:width-600 w-11/12 max-height-screen-90 overflow-y-auto dark:bg-black">
+
+							<div className="pt-4 pl-4">
+
+								<button onClick={() => setIsShowModal(false)} className="p-4 transition rounded-full hover:bg-zinc-100 dark:hover:bg-zinc-900">
+									<MdOutlineClose className="text-2xl text-zinc-500" />
+								</button>
+							</div>
+
+							<div className="px-8">
+
+								<h1 className="mt-4 text-2xl font-bold">新規Todo</h1>
+							</div>
+						</div>
+					</div>
+				}
+			</section>
+
+			<section>
 				{!isLoaded &&
 					<p>Loading ...</p>
 				}
@@ -59,7 +88,7 @@ function TodosSection() {
 						))}
 					</div>
 				}
-			</div>
+			</section>
 		</div>
 	)
 }
